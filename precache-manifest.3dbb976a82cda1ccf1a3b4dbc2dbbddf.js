04659348355121d1e34c11c5fbb97734ee6aaa9a
Sim <simtj1990@gmail.com>
2020-04-26 13:26:51 +0800

    Updates

---
+++ precache-manifest.3dbb976a82cda1ccf1a3b4dbc2dbbddf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "407d67e5262348bb47dfe3657dbf0910",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "6ec5efd4229d45da3eed",
+    "url": "/monsters-rolodex/static/css/main.aaf82d24.chunk.css"
+  },
+  {
+    "revision": "684a63fdcfa1f79a6337",
+    "url": "/monsters-rolodex/static/js/2.3ac13d0d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.3ac13d0d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6ec5efd4229d45da3eed",
+    "url": "/monsters-rolodex/static/js/main.57d9ab19.chunk.js"
+  },
+  {
+    "revision": "3acb30cb3387f9458b0a",
+    "url": "/monsters-rolodex/static/js/runtime-main.11c5fdd8.js"
+  }
+]);
